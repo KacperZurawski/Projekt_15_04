18de2b3c33a66482cef773663ea708eacfff2024
KacperZurawski <160867@stud.prz.edu.pl>
2021-04-15 16:10:24 +0200

    Wer 1.5

--- main.py
+++ main.py
@@ -3,25 +3,29 @@ import numpy as np
 import random as r
 
 
-def init_param(x_start, x_stop, node_num):
-    section_num = node_num - 1
-    nodes = np.zeros(node_num)
-    sections = np.zeros((section_num, 2))
+def init_param():
+    print('Write start x')
+    x_start = float(input())
+    print('Write end  x')
+    x_end = float(input())
     
-    nodes[0] = 0
-    for i in range(1,node_num):
-        value = round(r.uniform(x_start, x_stop),2)
-        nodes[i] = value
-        
-    for i in range(0,section_num):
-        values = r.sample(range(0,node_num), 2)
-        sections[i] = values
-    return nodes, sections
+    print('Write left condition')
+    left_cond = int(input())
+    print('Write right condition')
+    right_cond = int(input())
     
+    print('Write left type')
+    left_type = input()
+    print('Write right type')
+    right_type = input()
+    
+    x_range = np.array([x_start, x_end])
+    conditions = np.array([left_cond, right_cond])
+    types = np.array([left_type, right_type])
+    return x_range, conditions, types
     
 #1a
 
 #1b
-nodes, sections = init_param(0,1,4)
-print(nodes)
-print(sections)
+x_range, conditions, types = init_param()
+
