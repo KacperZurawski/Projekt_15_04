6666e313303d46cb05196cbbf561d894e12ec7a5
KacperZurawski <160867@stud.prz.edu.pl>
2021-04-15 13:31:07 +0200

    Wer 1.4

--- main.py
+++ main.py
@@ -2,33 +2,26 @@
 import numpy as np
 import random as r
 
-#globalne
-"""
-wezly = np.array(0,1,0.5,0.75)
-
-elementy = np.array([[1, 3],
-                     [4, 2],
-                     [3, 4]])
-#warunki brzegowe
-twb_L = 'D'
-twb_R = 'D'
-
-wwb_L = 0
-wwb_R = 1
-"""
 
 def init_param(x_start, x_stop, node_num):
+    section_num = node_num - 1
     nodes = np.zeros(node_num)
-    sections = np.zeros((node_num, 2))
+    sections = np.zeros((section_num, 2))
     
     nodes[0] = 0
     for i in range(1,node_num):
         value = round(r.uniform(x_start, x_stop),2)
         nodes[i] = value
+        
+    for i in range(0,section_num):
+        values = r.sample(range(0,node_num), 2)
+        sections[i] = values
     return nodes, sections
     
     
 #1a
 
 #1b
-print(init_param(0,1,4))
+nodes, sections = init_param(0,1,4)
+print(nodes)
+print(sections)
