4a1905845de4474e7d88e618581f2951bfa05f46
KacperZurawski <160867@stud.prz.edu.pl>
2021-04-15 13:16:45 +0200

    Wer 1.3

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import random as r
 
+#globalne
 """
-%% 1(a) inicjalizacja parametrow sterujacych programem CZESC 1
-[ parametry_sterujace ] = inicjalizacja_parametrow_sterujacych ( ... ) ;
-%% 1(b) definicja parametrow fizycznych i geometrycznych obszaru , warunkow brzegowych CZESC 1
-% - definicja przedzialu ,
-% - liczba wezlow / elementow modelu w przypadku dyskretyzacji jednorodnej
-% - zadanie funkcji wymuszajacej , parametrow rownania rozniczkowego
-% - ...
-[ parametry_geom_i_fiz ] = definicja_parametrow_geom_i_fiz ( ... ) ;
-"""
+wezly = np.array(0,1,0.5,0.75)
 
-def inicjalizacja_parametrow_sterujacych():
-    
-#1a
-[parametry_sterujace] = 
-
-
-#1b
-
-
-wezly = np.array([[1, 0],
-                  [2, 1],
-                  [3, 0.5],
-                  [4, 0.75]])
-
-elementy = np.array([[1, 1, 3],
-                     [2, 4, 2],
-                     [4, 3, 4]])
-
+elementy = np.array([[1, 3],
+                     [4, 2],
+                     [3, 4]])
+#warunki brzegowe
 twb_L = 'D'
 twb_R = 'D'
 
 wwb_L = 0
 wwb_R = 1
+"""
+
+def init_param(x_start, x_stop, node_num):
+    nodes = np.zeros(node_num)
+    sections = np.zeros((node_num, 2))
+    
+    nodes[0] = 0
+    for i in range(1,node_num):
+        value = round(r.uniform(x_start, x_stop),2)
+        nodes[i] = value
+    return nodes, sections
+    
+    
+#1a
+
+#1b
+print(init_param(0,1,4))
